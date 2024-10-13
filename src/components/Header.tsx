3b22ff77cdd4878a712f1e1c2523e448a783e681
KASMI7 <sunilkumarkasmi@gmail.com>
2024-10-13 17:44:37 +0530

    updated with memo and callbacks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ const Header: React.FC<HeaderProps> = ({ lastUser }) => {
   return (
     <header className='h-[10vh] w-full flex items-center justify-between px-5 bg-white shadow-md' style={{ position: 'sticky', top: 0, zIndex: 10 }}>
       <a href='/profile-form'>
-        <img src='/assets/ajmera-info.svg' alt='logo' className='w-40 h-30' />
+      <img src={`${process.env.PUBLIC_URL}/assets/ajmera-info.svg`} alt="Ajmera Infotech" />
       </a>
       {lastUser && (
         <div className='text-sm text-gray-600'>
